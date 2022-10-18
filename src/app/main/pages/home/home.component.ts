41ffc84d92fb0de6e45758b62823ff250087f988
Angel Barrios <ae.barrios@hotmail.com>
2022-10-18 18:26:04 -0400

    new: implement lazyload

--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterContentInit {
+  
+  showComponent:boolean = false
+  element: Element = document.createElement('div')
+  options: IntersectionObserverInit | undefined;
+  
+  ngAfterContentInit(): void {
+    this.getElement()
+  }
 
-  constructor() { }
+  getElement() {
+    this.element = document.querySelector("#about") || this.element
+    this.lazyLoading()
+  }
 
-  ngOnInit(): void {
+  lazyLoading() {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entrie => {
+        if (entrie.isIntersecting) {
+          this.showComponent = true
+          observer.unobserve(this.element)
+        }
+      })
+    }, this.options)
+    observer.observe(this.element)
   }
 
 }
